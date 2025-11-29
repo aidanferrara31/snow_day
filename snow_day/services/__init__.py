5eb959f21693b1cfdf2b429bb73a9c23c9b0075c
aidanferrara31 <anferrara3232@gmail.com>
2025-11-29 10:10:14 -0800

    Add local LLM server packaging and client wrapper

--- snow_day/services/__init__.py
+++ snow_day/services/__init__.py
@@ -1,1 +1,5 @@
 """Service layer utilities for snow day calculations."""
+
+from .llm_client import LLMClient, RuleBasedAdvisor, ScoredResort
+
+__all__ = ["LLMClient", "RuleBasedAdvisor", "ScoredResort"]
